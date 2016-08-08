2928355303823714b1257ff6f2078baab32b0f0f
Nat <nathan@jarvellis.com>
2016-08-08 21:27:36 +0100

    WIP

--- SimpleMonthlyCalculatorExport.user.js
+++ SimpleMonthlyCalculatorExport.user.js
@@ -8,7 +8,7 @@
 // @grant           GM_setValue
 // @grant           GM_addStyle
 // @grant           GM_getResourceText
-// @resource        CustomCSS http://gitlab.jarvellis.com/AWS/aws-smc-greasemonkey.css
+// @resource        CustomCSS https://s3-eu-west-1.amazonaws.com/jarvellis-css-share/aws-smc-greasemonkey.css
 // @include         https://calculator.s3.amazonaws.com/index.html
 // @require         https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
 // @updateURL	    http://gitlab.jarvellis.com/AWS/aws-smc-greasemonkey.user.js
@@ -19,8 +19,18 @@
 // http://wiki.greasespot.net/Include_and_exclude_rules
 // https://stackoverflow.com/questions/11275378/cross-domain-post-with-greasemonkey
 
+$("head").append (
+    '<link '
+  + 'href="https://s3-eu-west-1.amazonaws.com/jarvellis-css-share/aws-smc-greasemonkey.css" '
+  + 'rel="stylesheet" type="text/css">'
+);
+
 var script_version = GM_info.script.version; //script version to keep track of releases
 var timeout = 2000;
+
+// Add custom styling
+var fawsCSS = GM_getResourceText ("CustomCSS");
+GM_addStyle (fawsCSS);
 
 function run() {
     $(document).ready(function() {
@@ -31,10 +41,13 @@ function run() {
     });
 }
 
+// We need to wait until the UI has been generated before we start querying it
+setTimeout(run, timeout);
+
 function createStrip() {
     // Add FAWS export strip
     var fawsNode       = document.createElement ('div');
-    fawsNode.className = "fawsContainer";
+    fawsNode.setAttribute("id", "fawsContainer")
     fawsNode.innerHTML = '<button id="fawsButton" type="button">Export</button>';
     var parentNode = document.getElementById("aws-calculator").parentNode;
     var refNode = document.getElementById("aws-calculator");
@@ -53,5 +66,32 @@ function ButtonClickAction (fawsEvent) {
     document.getElementById ("fawsContainer").appendChild (fawsNode);
 }
 
-// We need to wait until the UI has been generated before we start querying it
-setTimeout(run, timeout);
+function AmazonEC2Instances(headers){
+    var headers = {
+        description:    {3: 'input'},
+        instances:      {6: 'input'},
+        usage_value:    {9: 'input'},
+        usage_type:     {10: 'select'},
+        instance_type:  {12: 'div'},
+        billing_option: {14: 'div'},
+        monthly_cost:   {17: 'div'}
+    }
+    row = tables[0].querySelector("tr").querySelectorAll('td')
+    // d = tables[0].querySelectorAll("tr")[1].querySelectorAll("td");
+    // d[9].querySelector("input").value
+    row.forEach(function(entry){
+        entry = entry.replace(/\s/g, "");
+        if (entry.length > 0) {
+            headers.push(row.innerText);
+        }
+    });
+    return headers
+}
+
+function collectDatasets(){
+    var tables = document.querySelectorAll ("table.itemsTable")
+    tables.forEach(function(entry){
+        console.log(entry);
+    });
+}
+
